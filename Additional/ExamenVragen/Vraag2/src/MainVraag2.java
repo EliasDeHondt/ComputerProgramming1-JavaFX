65fe7f37b6d09f33b833b009ba1009f5003bc344
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-24 21:58:43 +0100

    Commit Examen Vragen v1

---
+++ Additional/ExamenVragen/Vraag2/src/MainVraag2.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Elias De Hondt
+ * @since 24/03/2023
+ */
+
+public class MainVraag2 extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Vraag2 view = new Vraag2();
+        Scene scene = new Scene(view);
+        primaryStage.setTitle("Graphics");
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(300);
+        primaryStage.setWidth(300);
+        primaryStage.show(); // Antwoord
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
