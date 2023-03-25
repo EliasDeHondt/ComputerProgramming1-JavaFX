1ecee3bc5612e39acd0fb2bf96af6e0e1da0a56a
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-25 11:26:17 +0100

    Commit W16P3 "Engels" Exercise

--- W16P3/Engels/src/be/kdg/engels/view/EngelsView.java
+++ W16P3/Engels/src/be/kdg/engels/view/EngelsView.java
@@ -4,6 +4,8 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.layout.BorderPane;
 
 public class EngelsView extends BorderPane {
+    // 3.1 Voeg een attribuut van het type PieChart toe aan de klasse EngelsView
+    private PieChart pieChart;
 
     public EngelsView() {
         this.initialiseNodes();
@@ -11,11 +13,16 @@ public class EngelsView extends BorderPane {
     }
 
     private void initialiseNodes() {
-
+        // 3.3 Gebruik de default constructor van PieChart om je attribuut te initialiseren.
+        this.pieChart = new PieChart();
     }
 
     private void layoutNodes() {
-
+        // 3.4 Plaats je attribuut centraal op je BorderPane.
+        this.setCenter(this.pieChart);
     }
-
+    // 3.2 Schrijf een getter voor dit attribuut. Gebruik de juiste access level!
+    PieChart getPieChart() {
+        return this.pieChart;
+    }
 }
--- W16P3/Engels/src/be/kdg/engels/view/Presenter.java
+++ W16P3/Engels/src/be/kdg/engels/view/Presenter.java
@@ -15,14 +15,25 @@ public class Presenter {
         this.model = model;
         this.view = view;
 
-        this.addEventHandlers();
         this.updateView();
     }
 
-    private void addEventHandlers() {
-    }
-
     private void updateView() {
+        // 4.2 Maak een ObservableList aan van PieChart.Data objecten. Dit kan je doen met behulp van de klassemethode observableArrayList van de klasse FXCollections.
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        // 4.1 Roep de getData methode op van het model om de te tonen gegevens te verkrijgen.
+        // 4.3 Doorloop de gegevens die je vanuit het model hebt opgevraagd en voeg voor elke entry uit deze Map een element toe aan je ObservableList:
+        for (Map.Entry<String, Double> data : this.model.getData().entrySet()) {
+            // 4.4 Gebruik de add methode van ObservableList. Deze methode verwacht telkens een parameter van het type PieChart.Data.
+            pieChartData.add(new PieChart.Data(data.getKey(), data.getValue()));
+        }
+
+        // 4.5 Gebruik de setData methode van PieChart om je ObservableList door te geven aan je pie chart.
+        this.view.getPieChart().setData(pieChartData);
+
+        // 4.6 Zet de titel van je pie chart op “Hoe je de Engels taal leert”.
+        this.view.getPieChart().setTitle("Hoe je de Engels taal leert");
 
     }
 }
