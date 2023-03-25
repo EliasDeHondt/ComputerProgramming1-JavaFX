a49ce58d622959b3acd03b7e30250b8d31849284
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-25 16:17:51 +0100

    Commit Vraag5 Exercise

---
+++ Additional/ExamenVragen/Vraag5/src/MainVraag5.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * @author Elias De Hondt
+ * @since 24/03/2023
+ */
+
+public class MainVraag5 extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        MyView view = new MyView();
+        primaryStage.setTitle("Buttons");
+        primaryStage.setScene(new Scene(view));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+class MyView extends VBox {
+    private final static String[] text = {"Koen", "Paula", "Evy", "Katrien"};
+    private final Button[] buttons = new Button[text.length];
+
+    public MyView() {
+        this.initialiseNodes();
+        this.layoutNodes();
+    }
+
+    private void initialiseNodes() {
+        for (int i = 0; i < text.length; i++) {
+            this.buttons[i] = new Button(text[i]);
+            this.getChildren().add(this.buttons[i]); // voeg knoppen toe aan View-object
+        }
+    }
+
+    private void layoutNodes() {
+        this.setPadding(new Insets(40));
+        this.setSpacing(10); // voeg deze regel toe om ruimte tussen de knoppen in te stellen
+        for (int i = 0; i < text.length; i++) {
+            this.buttons[i].setAlignment(Pos.CENTER);
+            this.buttons[i].setPrefWidth(80);
+            this.buttons[i].setPrefHeight(40);
+        }
+    }
+}
---
+++ Additional/ExamenVragen/Vraag6/src/Launcher.java
@@ -0,0 +1,10 @@
+/**
+ * @author Elias De Hondt
+ * @since 24/03/2023
+ */
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainVraag6.main(args);
+    }
+}
